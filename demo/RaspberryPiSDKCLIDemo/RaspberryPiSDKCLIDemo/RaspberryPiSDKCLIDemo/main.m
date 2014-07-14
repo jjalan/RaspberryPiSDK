0e52ddf5fed9e1993903f1484a239113d3097a86
Jaikishan Jalan <jai.ism@gmail.com>
2014-07-13 18:24:42 -0700

    Initial Commit

---
+++ demo/RaspberryPiSDKCLIDemo/RaspberryPiSDKCLIDemo/RaspberryPiSDKCLIDemo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RaspberryPiSDKCLIDemo
+//
+//  Created by Jaikishan Jalan on 7/13/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
